3538fae0b7c15926163c81c17e34e9c70bfa8d4c
Carlos Vega <106979457+carlosvegab@users.noreply.github.com>
2022-06-06 16:41:43 +0200

    Tests examples added

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Cvega\Mainmenu\Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Cvega\Mainmenu\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
